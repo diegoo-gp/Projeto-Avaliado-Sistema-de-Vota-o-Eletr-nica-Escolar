f94a24db54b7ef4e1a03882ce0ea6080a5125c45
diego <diego.albino.1904@gmail.com>
2026-02-26 16:37:36 +0000

    Novo validation.js
    Novo ficheiro que valida os votos e os votantes.

--- voting-system/validation.js
+++ voting-system/validation.js
@@ -1,21 +1,6 @@
-// Este arquivo é responsável por conter funções de validação. Ele contém as seguintes funções: 
-// - **voterAlreadyVoted(voters, voterId)**: Verifica se um eleitor já votou, comparando o ID do eleitor com uma lista de eleitores que já votaram.
-// - **candidateExists(names, candidateName)**: Verifica se um candidato existe, comparando o nome do candidato com uma lista de nomes de candidatos.
-// - **getCandidateIndex(names, candidateName)**: Retorna o índice de um candidato na lista de nomes, ou -1 se não encontrado.
-
-
 function voterAlreadyVoted(voters, voterId) {
     for (let i = 0; i < voters.length; i++) {
         if (voters[i] === voterId) {
-            return true
-        }
-    }
-    return false
-}
-
-function candidateExists(names, candidateName) {
-    for (let i = 0; i < names.length; i++) {
-        if (names[i] === candidateName) {
             return true
         }
     }
@@ -33,6 +18,5 @@ function getCandidateIndex(names, candidateName) {
 
 export {
     voterAlreadyVoted,
-    candidateExists,
     getCandidateIndex
 }
