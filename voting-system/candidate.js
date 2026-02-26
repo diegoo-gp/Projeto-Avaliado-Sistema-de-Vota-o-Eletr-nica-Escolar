6145fe38e12e4cc269ccc34cfd394b30967628b5
diego <diego.albino.1904@gmail.com>
2026-02-26 16:36:31 +0000

    Novo candidate.js
    Novo ficheiro do candidate.js a funcionar.

--- voting-system/candidate.js
+++ voting-system/candidate.js
@@ -1,13 +1,10 @@
-// Este arquivo é responsável por criar os candidatos e gerenciar os votos de cada um. Ele contém as seguintes funções: 
-// - **createCandidates(names)**: Cria um array de votos inicializado com 0 para cada candidato, com base em uma lista de nomes fornecida.
-// - **addVote(votes, index)**: Incrementa o número de votos para um candidato específico, identificado pelo índice no array de votos.
-
 function createCandidates(names) {
-    // Cria array de votos com 0 para cada candidato
     const votes = []
+
     for (let i = 0; i < names.length; i++) {
         votes.push(0)
     }
+
     return votes
 }
 
